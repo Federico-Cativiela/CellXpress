b284fef546c5eba607c43a4a990e83f5a0bc7cf7
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-08-02 18:21:14 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const userRoutes = require("./routes/user")
 //middlewares
 app.use(express.json())
 app.use(morgan("dev"));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://localhost:3002" }));
+// agregado por mi 
+app.use(cors({ origin: "http://localhost:5173" }));
 //routes
 app.use("/", userRoutes);
 app.use('/products', productRoutes)
