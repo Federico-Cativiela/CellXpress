c8fb11bf0cc1d957cdc11eb1822612260e6f2882
Juan191101 <ruartejuancruz@gmail.com>
2023-08-03 09:23:05 -0300

    filtrosMarcaYPrecios

--- src/routes/product.js
+++ src/routes/product.js
@@ -37,6 +37,30 @@ router.get("/", (req, res) => {
   });
 
 
+// Ruta para obtener celulares por marca y rango de precios
+router.get("/brand/:brand", (req, res) => {
+  const brand = req.params.brand;
+  const minPrice = req.query.minPrice;
+  const maxPrice = req.query.maxPrice;
+
+  // Crear un objeto de consulta para filtrar por marca y rango de precios
+  const query = { brand: brand.toUpperCase() };
+
+  // Agregar el filtro de rango de precios si se proporcionan minPrice y maxPrice válidos
+  if (minPrice && !isNaN(parseFloat(minPrice)) && maxPrice && !isNaN(parseFloat(maxPrice))) {
+    query.price = { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) };
+  } else if (minPrice && !isNaN(parseFloat(minPrice))) {
+    query.price = { $gte: parseFloat(minPrice) };
+  } else if (maxPrice && !isNaN(parseFloat(maxPrice))) {
+    query.price = { $lte: parseFloat(maxPrice) };
+  }
+
+  product
+    .find(query)
+    .then((filteredProducts) => res.json({ products: filteredProducts }))
+    .catch((error) => res.status(400).json({ error: "Error al obtener los celulares por marca y rango de precios" }));
+});
+
 
 // Ruta para crear un nuevo producto
 router.post("/", (req, res) => {
