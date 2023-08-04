c0156a1eeea0678f432aa21bb17f1000cd9bba7b
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-08-04 10:56:22 -0300

    DeployFrontCors-Fede

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const userRoutes = require("./routes/user")
 //middlewares
 app.use(express.json())
 app.use(morgan("dev"));
-app.use(cors({ origin: "http://127.0.0.1:5173" }));
+app.use(cors({ origin: "https://pf-cell-xpress-frontend.vercel.app/" }));
 //routes
 app.use("/", userRoutes);
 app.use('/products', productRoutes)
