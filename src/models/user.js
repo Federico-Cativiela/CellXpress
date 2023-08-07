df3f8205ec608fc9648b3157d4a6a28c427a5b3c
Juan191101 <ruartejuancruz@gmail.com>
2023-08-07 12:24:31 -0300

    usuarios v2 y carrito v1

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = mongoose.Schema({
+  UID:{
+    type:String,
+    default: uuidv4,
+  },
   name: {
     type: String,
     required: true,
@@ -12,11 +17,22 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     requires: true,
+    unique: true
   },
   password: {
     type: String,
     required: true,
   },
+  admin:{
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+},{
+  versionKey: false
 });
 
 module.exports = mongoose.model("User", userSchema);
