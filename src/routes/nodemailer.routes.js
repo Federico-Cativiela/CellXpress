b6da9264d209fff5be424b43c50fb1c0205fdf65
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-08-09 11:59:20 -0300

    nodemailer-routes+config

---
+++ src/routes/nodemailer.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const sendEmail = require('../controllers/nodemailer');
+const sendEmailOrder = require("../controllers/nodemailerOrder");
+const sendEmailRegister= require("../controllers/nodemailerRegister");
+
+const router = express.Router();
+
+router.post("/contacto", sendEmail);
+router.post("/orden-de-compra", sendEmailOrder)
+router.post("/register", sendEmailRegister)
+
+module.exports = router;
