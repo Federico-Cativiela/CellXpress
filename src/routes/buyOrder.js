b1c91452034b1373893e79659bcfef9c346369f1
GeroGambuli <gerogambuli2002@gmail.com>
2023-08-15 17:39:46 -0300

    arreglando userid

--- src/routes/buyOrder.js
+++ src/routes/buyOrder.js
@@ -4,7 +4,9 @@ const BuyOrder = require("../models/buyOrder");
 const Product = require("../models/product");
 const User = require("../models/user");
 const postMailerOrder = require("../controllers/nodemailerOrder");
-const stripe = require("stripe")("sk_test_51NccbYLQEdx2wACSJ21hlx0Y1Bx9j5eKHRyJqAnIjInB32qgNGW76bkPdP3Qt7JbcFc6UCaRkAV8LVhetHRAyRjx00SIUry2yX");
+const stripe = require("stripe")(
+  "sk_test_51NccbYLQEdx2wACSJ21hlx0Y1Bx9j5eKHRyJqAnIjInB32qgNGW76bkPdP3Qt7JbcFc6UCaRkAV8LVhetHRAyRjx00SIUry2yX"
+);
 
 // Agregar un producto al carrito
 router.post("/add-to-cart", async (req, res) => {
@@ -22,7 +24,9 @@ router.post("/add-to-cart", async (req, res) => {
 
     // Verifica si hay suficiente cantidad disponible del producto
     if (product.count < quantity) {
-      return res.status(400).json({ message: "Cantidad insuficiente de producto." });
+      return res
+        .status(400)
+        .json({ message: "Cantidad insuficiente de producto." });
     }
 
     // Resta la cantidad del producto
@@ -65,15 +69,14 @@ router.post("/add-to-cart", async (req, res) => {
   }
 });
 
-
-
-
 // Ver el contenido del carrito
 router.get("/cart/:userId", async (req, res) => {
   const { userId } = req.params;
 
   try {
-    const cart = await BuyOrder.findOne({ userId }).populate("products.product");
+    const cart = await BuyOrder.findOne({ userId }).populate(
+      "products.product"
+    );
 
     res.json(cart);
   } catch (error) {
@@ -84,55 +87,57 @@ router.get("/cart/:userId", async (req, res) => {
 
 // Modificar la cantidad de productos en el carrito
 router.put("/update-cart/:userId/:productId", async (req, res) => {
-    const { userId, productId } = req.params;
-    const { quantity } = req.body;
-  
-    try {
-      const existingCart = await BuyOrder.findOne({ userId });
-  
-      if (!existingCart) {
-        return res.status(404).json({ message: "Carrito no encontrado." });
-      }
-  
-      const existingProduct = existingCart.products.find(
-        (item) => item.product.toString() === productId
-      );
-  
-      if (!existingProduct) {
-        return res.status(404).json({ message: "Producto no encontrado en el carrito." });
-      }
-  
-      const product = await Product.findById(productId);
-  
-      if (!product) {
-        return res.status(404).json({ message: "Producto no encontrado." });
-      }
-  
-      const originalQuantity = existingProduct.quantity;
-      const productPrice = product.price;
-  
-      // Restablece la cantidad del producto en la base de datos antes de modificar el carrito
-      product.count += originalQuantity;
-      product.count -= quantity;
-  
-      // Actualiza el producto en la base de datos
-      await product.save();
-  
-      // Actualiza la cantidad en el carrito
-      existingProduct.quantity = quantity;
-  
-      // Actualiza el total del carrito
-      existingCart.total += (quantity - originalQuantity) * productPrice;
-  
-      // Guarda los cambios en el carrito y devuelve la respuesta
-      const updatedCart = await existingCart.save();
-      res.json(updatedCart);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Hubo un error en el servidor." });
+  const { userId, productId } = req.params;
+  const { quantity } = req.body;
+
+  try {
+    const existingCart = await BuyOrder.findOne({ userId });
+
+    if (!existingCart) {
+      return res.status(404).json({ message: "Carrito no encontrado." });
     }
-  });
-  
+
+    const existingProduct = existingCart.products.find(
+      (item) => item.product.toString() === productId
+    );
+
+    if (!existingProduct) {
+      return res
+        .status(404)
+        .json({ message: "Producto no encontrado en el carrito." });
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Producto no encontrado." });
+    }
+
+    const originalQuantity = existingProduct.quantity;
+    const productPrice = product.price;
+
+    // Restablece la cantidad del producto en la base de datos antes de modificar el carrito
+    product.count += originalQuantity;
+    product.count -= quantity;
+
+    // Actualiza el producto en la base de datos
+    await product.save();
+
+    // Actualiza la cantidad en el carrito
+    existingProduct.quantity = quantity;
+
+    // Actualiza el total del carrito
+    existingCart.total += (quantity - originalQuantity) * productPrice;
+
+    // Guarda los cambios en el carrito y devuelve la respuesta
+    const updatedCart = await existingCart.save();
+    res.json(updatedCart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Hubo un error en el servidor." });
+  }
+});
+
 // Eliminar un producto del carrito
 router.delete("/remove-from-cart/:userId/:productId", async (req, res) => {
   const { userId, productId } = req.params;
@@ -149,7 +154,9 @@ router.delete("/remove-from-cart/:userId/:productId", async (req, res) => {
     );
 
     if (!existingProduct) {
-      return res.status(404).json({ message: "Producto no encontrado en el carrito." });
+      return res
+        .status(404)
+        .json({ message: "Producto no encontrado en el carrito." });
     }
 
     const product = await Product.findById(productId);
@@ -191,43 +198,47 @@ router.get("/success/:userId", async (req, res) => {
     const pendingCart = await BuyOrder.findOne({ userId, status: "pending" });
 
     if (!pendingCart) {
-      return res.status(404).json({ message: "No se encontró ninguna orden de compra pendiente." });
+      return res
+        .status(404)
+        .json({ message: "No se encontró ninguna orden de compra pendiente." });
     }
 
     // Cambia el estado de la orden a "success"
     pendingCart.status = "success";
     await pendingCart.save();
 
-    res.send("Compra exitosa. Tu orden de compra ha sido actualizada a 'success'.");
+    res.send(
+      "Compra exitosa. Tu orden de compra ha sido actualizada a 'success'."
+    );
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error al mostrar el mensaje de éxito." });
   }
 });
 
-
-
-
 // Ruta para realizar el pago
 router.post("/checkout", async (req, res) => {
-  const { userId } = req.body;
-  
+  const { userId } = req.query;
+
+  console.log(userId);
   try {
     // Buscar el carrito pendiente del usuario
     // const user = await User.findById(userId)
-    const cart = await BuyOrder.findOne({ userId, status: "pending" }).populate("products.product");
+    const cart = await BuyOrder.findOne({ userId, status: "pending" }).populate(
+      "products.product"
+    );
 
     if (!cart) {
-      return res.status(404).json({ message: "Carrito no encontrado o ya se realizó una compra exitosa." });
+      return res.status(404).json({
+        message: "Carrito no encontrado o ya se realizó una compra exitosa.",
+      });
     }
-
-   
 
     // Crear una lista de productos para la API de Stripe
     const lineItems = cart.products.map((item) => ({
       price_data: {
         currency: "usd",
-        unit_amount: Math.floor(item.product.price * 100 / 500), // Dividir por el valor del dolar
+        unit_amount: Math.floor((item.product.price * 100) / 500), // Dividir por el valor del dolar
         product_data: {
           name: item.product.title, // Nombre del producto
           description: item.product.description, // Descripción del producto
@@ -253,14 +264,13 @@ router.post("/checkout", async (req, res) => {
     //   )).join("\n")}
     //   Total: $${successCart.total}
     //   Fecha: ${successCart.date}
-      
+
     //   ¡Esperamos verte nuevamente pronto!
     // `;
 
     // const sendEmail = await postMailerOrder(user)
 
     // console.log(sendEmail)
-    
 
     const paymentLink = session.url;
     res.json({ sessionId: session.id, paymentLink: paymentLink });
@@ -269,7 +279,6 @@ router.post("/checkout", async (req, res) => {
     res.status(500).json({ message: "Hubo un error en el servidor" });
   }
 });
-
 
 router.get("/all", async (req, res) => {
   try {
@@ -280,6 +289,5 @@ router.get("/all", async (req, res) => {
     res.status(500).json({ message: "Hubo un error en el servidor." });
   }
 });
-
 
 module.exports = router;
