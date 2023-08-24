f54dc53af862df286f29237418d86b9c6dca868b
Federico-Cativiela <108641770+Federico-Cativiela@users.noreply.github.com>
2023-08-23 22:37:13 -0300

    estilosRegistro

--- src/controllers/nodemailerRegister.js
+++ src/controllers/nodemailerRegister.js
@@ -7,10 +7,22 @@ const postMailerRegister = async function(newUser){
         from: "infocxps@gmail.com",
         to:newUser.email,
         subject:"Registro en CellXpress",
-        html:`
+        html:`<Style>
+        #logoimg{
+            width: 150px;
+            height: 150px;
+            margin: 20px;
+            left: 15px;
+            position: absolute;
+            display: flex;
+            align-items: start;
+            justify-content: start;
+            border-radius: 100%;
+        }
+        </Style>
         <b>Gracias por confiar en nosotros ,te has registrado exitosamente!</b>
-        <img src="https://res.cloudinary.com/djqwbu0my/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1691088616/OIP_mzc6jr.jpg" alt="Bienvenido a CellXpress" />
-        `
+        <img id="logoimg" src="https://res.cloudinary.com/djqwbu0my/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1691088616/OIP_mzc6jr.jpg" alt="Bienvenido a CellXpress" />
+           `
     })
     return email
 }
